41379f5b37a8f2b198c70a4ebe214bb5bf476b5f
Jon Kinney <jonkinney@gmail.com>
2012-05-03 21:52:59 -0500

    Update lib/deploy_and_deliver.rb

--- lib/deploy_and_deliver.rb
+++ lib/deploy_and_deliver.rb
@@ -1,24 +1,27 @@
-require 'pivotal-tracker'
-
-module DeployAndDeliver
-  class Project
-    attr_reader :project
-
-    def initialize(context)
-      PivotalTracker::Client.use_ssl = context[:pivotal_tracker_ssl]
-      PivotalTracker::Client.token = context[:pivotal_tracker_token]
+require 'pivotal-tracker'                                                       
+                                                                                
+module DeployAndDeliver                                                         
+  class Project                                                                 
+    attr_reader :project                                                        
+                                                                                
+    def initialize(context)                                                     
+      PivotalTracker::Client.use_ssl = context[:pivotal_tracker_ssl]            
+      PivotalTracker::Client.token = context[:pivotal_tracker_token]            
       @project = PivotalTracker::Project.find(context[:pivotal_tracker_project_id])
-    end
-
-    def deliver_and_report
-      stories = project.stories.all(:current_state => 'finished')
-      stories.each{|story| story.update :current_state => 'delivered'}
-
+    end                                                                         
+                                                                                
+    def deliver_and_report                                                      
+      stories = project.stories.all(:current_state => 'finished')               
+      stories.each do |story|                                                   
+        story.update :labels => story.labels += ",staging"                      
+        story.update :current_state => 'delivered'                              
+      end                                                                       
+                                                                                
       puts "* delivered #{stories.size} stories (#{stories.map(&:estimate).inject(:+)} points)"
-      stories.each do |story|
+      stories.each do |story|                                                   
         puts "  - #{story.story_type.capitalize}: #{story.name} (#{story.estimate} #{story.estimate == 1 ? "point" : "points"})"
-      end
-    end
-  end
-
-end
+      end                                                                       
+    end                                                                         
+  end                                                                           
+                                                                                
+end        

46ef9e504bef7fae58935f0c670725036fe09b3a
Jon Kinney <jonkinney@gmail.com>
2012-05-24 08:30:11 -0500

    Inject label on deploy
    - TODO: make this configurable

--- lib/deploy_and_deliver.rb
+++ lib/deploy_and_deliver.rb
@@ -12,7 +12,13 @@ module DeployAndDeliver
 
     def deliver_and_report
       stories = project.stories.all(:current_state => 'finished')
-      stories.each{|story| story.update :current_state => 'delivered'}
+      stories.each do |story|
+        if "#{story.labels}".length > 0
+          delimiter = ","
+        end
+        story.update :labels => "on staging#{delimiter}#{story.labels}"
+        story.update :current_state => 'delivered'
+      end
 
       puts "* delivered #{stories.size} stories (#{stories.map(&:estimate).inject(:+)} points)"
       stories.each do |story|
